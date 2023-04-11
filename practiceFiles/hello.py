885ed3804d018b59da8fa4734a3f7603e627b962
Henry Cho <34312635+hankcho@users.noreply.github.com>
2023-04-11 12:44:30 +0000

    removed practiceFiles because I copied those tiles to the new OtherPracticeFiles folder

--- practiceFiles/hello.py
+++
@@ -1,8 +0,0 @@
-def main():
-    print("Hello")
-    name = input("what is your name? ")
-    print("nice to meet you", name)
-
-
-if __name__ == "__main__":
-    main()
--- practiceFiles/import_modules.py
+++
@@ -1,6 +0,0 @@
-import math
-
-print("the squareroot of 16 is" , math.sqrt(16))
-
-print("Pi is", math.pi)
-
